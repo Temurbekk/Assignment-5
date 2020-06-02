e84f20132e2802a1946022ac714b171241cf28e0
Derrick <derrick.chow234@gmail.com>
2020-06-02 09:19:45 -0400

    Now you can view which color is selected

--- script.js
+++ script.js
@@ -1,126 +1,133 @@
 let columns = 0;
 let colouring = false;
 var selectedColor = "#FFFFFF";
+var colorPicker = document.getElementById("colorPicker");
 
 function pickBlue() {
-  selectedColor = "#00FFFF";
+    selectedColor = "#00FFFF";
+    colorPicker.textContent = "Color Selected: Blue"
+
 }
 
 function pickRed() {
-  selectedColor = "#FF6666";
+    selectedColor = "#FF6666";
+    colorPicker.textContent = "Color Selected: Red"
+
 }
 
 function pickWhite() {
-  selectedColor = "#FFFFFF";
+    selectedColor = "#FFFFFF";
+    colorPicker.textContent = "Color Selected: White"
+
 }
 
 const tableCells = document.getElementsByTagName("td");
 
 const addRow = () => {
-  if (columns === 0) columns = 1;
-  const table = document.getElementById("table");
-  const row = document.createElement("tr");
+    if (columns === 0) columns = 1;
+    const table = document.getElementById("table");
+    const row = document.createElement("tr");
 
-  row.classList.add("row");
-  table.appendChild(row);
+    row.classList.add("row");
+    table.appendChild(row);
 
-  let index = 0;
+    let index = 0;
 
-  while (index < columns) {
-    const pixel = document.createElement("td");
-    pixel.classList.add("pixel");
-    row.appendChild(pixel);
-    index++;
-  }
+    while (index < columns) {
+        const pixel = document.createElement("td");
+        pixel.classList.add("pixel");
+        row.appendChild(pixel);
+        index++;
+    }
 
-  for (let i = 0; i < tableCells.length; i++) {
-    //Derrick's Code: This allows you to change the color of the cells
-    //updated to call a general event handling function to allow for setting
-    //colours via drag
-    handleEvents(tableCells[i])
-  }
-  //Making sure the function is being called
-  console.log("Adding cell");
+    for (let i = 0; i < tableCells.length; i++) {
+        //Derrick's Code: This allows you to change the color of the cells
+        //updated to call a general event handling function to allow for setting
+        //colours via drag
+        handleEvents(tableCells[i])
+    }
+    //Making sure the function is being called
+    console.log("Adding cell");
 };
 
 const addColumn = () => {
-  if (columns === 0) {
-    column = 1;
-    addRow();
-  }
-  columns++;
-  const row = document.getElementsByClassName("row");
-  const tableRow = Array.from(row);
+    if (columns === 0) {
+        column = 1;
+        addRow();
+    }
+    columns++;
+    const row = document.getElementsByClassName("row");
+    const tableRow = Array.from(row);
 
-  tableRow.forEach((cell) => {
-    const pixel = document.createElement("td");
-    pixel.classList.add("pixel");
-    cell.appendChild(pixel);
-  });
+    tableRow.forEach((cell) => {
+        const pixel = document.createElement("td");
+        pixel.classList.add("pixel");
+        cell.appendChild(pixel);
+    });
 
-  for (let i = 0; i < tableCells.length; i++) {
-    //Derrick's Code: This allows you to change the color of the cells
-    //updated to call a general event handling function to allow for setting
-    //colours via drag
-    handleEvents(tableCells[i])
-  }
+    for (let i = 0; i < tableCells.length; i++) {
+        //Derrick's Code: This allows you to change the color of the cells
+        //updated to call a general event handling function to allow for setting
+        //colours via drag
+        handleEvents(tableCells[i])
+    }
 
-  //Making sure the function is being called
-  console.log("Adding column");
+    //Making sure the function is being called
+    console.log("Adding column");
 };
 const removeRow = () => {
-  const row = document.getElementsByClassName("row");
-  const tableRow = Array.from(row);
-  let length = tableRow.length - 1;
-  tableRow[length].parentNode.removeChild(tableRow[length]);
+    const row = document.getElementsByClassName("row");
+    const tableRow = Array.from(row);
+    let length = tableRow.length - 1;
+    tableRow[length].parentNode.removeChild(tableRow[length]);
 
-  console.log("Removing Row");
+    console.log("Removing Row");
 };
 
 const removeColumn = () => {
-  columns--;
+    columns--;
 
-  const row = document.getElementsByClassName("row");
-  const tableRow = Array.from(row);
+    const row = document.getElementsByClassName("row");
+    const tableRow = Array.from(row);
 
-  tableRow.forEach((cell) => {
-    cell.removeChild(cell.lastChild);
-  });
+    tableRow.forEach((cell) => {
+        cell.removeChild(cell.lastChild);
+    });
 
-  console.log("Remove Column");
+    console.log("Remove Column");
 };
 const selectColour = (colour) => {
-  currentColour = colour;
+    currentColour = colour;
 };
 
 const setColour = (cell) => {
-  cell.style.backgroundColour = selectedColor;
-  cell.classList.remove("uncoloured");
+    cell.style.backgroundColour = selectedColor;
+    cell.classList.remove("uncoloured");
 };
 
 function handleEvents(cell) {
-  //handles clicking
-  cell.addEventListener("click", setColour(cell));
+    //handles clicking
+    cell.addEventListener("click", setColour(cell));
 
-  //handles dragging/"drawing"
-  cell.addEventListener("mousedown", (e) => {
-    colouring = true;
-  });
+    //handles dragging/"drawing"
+    cell.addEventListener("mousedown", (e) => {
+        colouring = true;
+    });
 
-  cell.addEventListener("mousemove", (e) => {
-    if (colouring)
-      event.target.style.backgroundColor = selectedColor;
-  });
+    cell.addEventListener("mousemove", (e) => {
+        if (colouring)
+            event.target.style.backgroundColor = selectedColor;
+    });
 
-  cell.addEventListener("mouseup", (e) => {
-    if (colouring)
-      colouring = false;
-  });
+    cell.addEventListener("mouseup", (e) => {
+        if (colouring)
+            colouring = false;
+    });
 }
 
 const clearAll = () => {
-  const allCells = document.getElementsByTagName("td");
-  for (let i = 0; i < allCells.length; i++) {
-    allCells[i].style.backgroundColor = "";
-  }
+    const allCells = document.getElementsByTagName("td");
+    for (let i = 0; i < allCells.length; i++) {
+        allCells[i].style.backgroundColor = "";
+    }
 };
